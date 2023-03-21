1a9df13327c18df74504b84c835cb32355481899
SvetlanaMikheeva <80316217+SvetlanaMikheeva@users.noreply.github.com>
2023-03-21 13:50:39 +0400

    Update CustomValidator.cs

--- CS/Pages/CustomValidator.cs
+++ CS/Pages/CustomValidator.cs
@@ -2,43 +2,52 @@ using System;
 using System.Collections.Generic;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Forms;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace CustomValidation.Pages {
+    public class ValidationMessageStoreEventArgs : EventArgs {
+        public ValidationMessageStoreEventArgs(object model, Dictionary<string, List<string>> errors) {
+            EditModel = model;
+            Errors = errors;
+        }
+        public object EditModel { get; }
+        private Dictionary<string, List<string>> Errors { get; }
+        public void AddError(string fieldName, string errorText) {
+            if (string.IsNullOrEmpty(fieldName) || string.IsNullOrEmpty(errorText))
+                return;
+            if (!Errors.ContainsKey(fieldName))
+                Errors[fieldName] = new List<string>();
+            Errors[fieldName].Add(errorText);
+        }
+    }
     public class MyCustomValidator : ComponentBase {
-        private ValidationMessageStore messageStore;
-
+        private ValidationMessageStore MessageStore;
         [CascadingParameter]
         private EditContext CurrentEditContext { get; set; }
-
         [Parameter]
-        public Action<EditContext> DataItemValidating { get; set; }
-
+        public Action<ValidationMessageStoreEventArgs> DataItemValidating { get; set; }
         protected override void OnInitialized() {
-            if(CurrentEditContext == null) {
+            if (CurrentEditContext == null) {
                 throw new InvalidOperationException(
                     $"{nameof(MyCustomValidator)} requires a cascading " +
                     $"parameter of type {nameof(EditContext)}. " +
                     $"For example, you can use {nameof(MyCustomValidator)} " +
                     $"inside an {nameof(EditForm)}.");
             }
-
-            messageStore = new(CurrentEditContext);
-
-            CurrentEditContext.OnValidationRequested += (s, e) =>
-                DataItemValidating.Invoke(CurrentEditContext);
-            CurrentEditContext.OnFieldChanged += (s, e) =>
-                DataItemValidating.Invoke(CurrentEditContext);
+            MessageStore = new(CurrentEditContext);
+            CurrentEditContext.OnValidationRequested += (s, e) => ValidateData();
+            CurrentEditContext.OnFieldChanged += (s, e) => ValidateData();
         }
+        void ValidateData() {
+            if (DataItemValidating == null)
+                return;
+            var errors = new Dictionary<string, List<string>>();
+            var args = new ValidationMessageStoreEventArgs(CurrentEditContext.Model, errors);
+            DataItemValidating.Invoke(args);
 
-        public void DisplayErrors(Dictionary<string, List<string>> errors) {
-            foreach(var err in errors) {
-                messageStore.Add(CurrentEditContext.Field(err.Key), err.Value);
-            }
-            CurrentEditContext.NotifyValidationStateChanged();
-        }
-
-        public void ClearErrors() {
-            messageStore.Clear();
+            MessageStore.Clear();
+            foreach (var error in errors)
+                MessageStore.Add(CurrentEditContext.Field(error.Key), error.Value);
             CurrentEditContext.NotifyValidationStateChanged();
         }
     }
